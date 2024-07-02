540d1d0b28d1774457bb3c73b46b6af64e37026b
Ragul_Jeevan <raguljeevan8@gmail.com>
2024-07-02 22:30:28 +0530

    bootstrap 5 instlled and side nav bar added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeLayoutComponent } from './components/home-layout/home-layout.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeLayoutComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/home-layout/home-layout.component.ts
+++ src/app/components/home-layout/home-layout.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  route?: string;
+  subMenu?: MenuItem[];
+  collapseId?: string;
+}
+
 
 @Component({
   selector: 'track-home-layout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MaterialModule, RouterOutlet],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.scss'
 })
-export class HomeLayoutComponent {
+export class HomeLayoutComponent implements OnInit {
+
+  public menuItems: MenuItem[] = [
+    { name: 'Home', icon: 'fas fa-home', route: '/home',subMenu: [], },
+    { name: 'Dashboard', icon: 'fas fa-tachometer-alt', collapseId: 'submenu1',subMenu: [],},
+    { name: 'Core', icon: 'fas fa-shopping-cart', route: '/orders',subMenu: [], },
+    {name: 'Foundation', icon: 'fas fa-layer-group',collapseId: 'submenu2',subMenu: [],},
+    {name: 'User-Management', icon: 'fas fa-layer-group',collapseId: 'submenu2',subMenu: [],},
+  ];
+
+  constructor(
+    private route: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  public navigateTo(route: any) {
+    // this.route.navigate([route]);
+
+  }
+
 
 }
